b512434b797cb9a915d742a786ee76d4923322ed
Rajdeep Ghosh <rikta_raj@rediffmail.com>
2021-01-06 16:44:57 +0530

    StatusCode Added

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ const { doOCR, searchKey } = require('../utilities/utilities')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.statusCode=200;
+        res.statusCode=200;
         res.setHeader('Content-Type','application/json');
         res.json({status:'Success',data:[]});
 });
@@ -20,14 +20,13 @@ router.get('/', function(req, res, next) {
 /* Key Generation */
 router.route('/generate')
 .get((req,res,next) => {
-        res.statusCode=200;
+        res.statusCode=400; //Bad request 400 as post operation not allowed here
         res.setHeader('Content-Type','application/json');
         result="Please Do Post Here with 'name' and 'email' ";
         res.json({status:'Failure',data:result});
 })
 .post(async (req, res, next) => {
         req.props = Object.assign(req.query, req.params, req.body);
-        res.statusCode=200;
         res.setHeader('Content-Type','application/json');
         let result='';
         console.log(req.props);
@@ -39,6 +38,7 @@ router.route('/generate')
           
           // If Email already exists, return error
           if (emailData) {
+            res.statusCode=401; //Unauthorized 401 as the credentials are already in use
             return res.json({ status: 'Failure', data: 'E-mail already exists.' });
           }
           
@@ -48,10 +48,12 @@ router.route('/generate')
             key
           });
           await data.save();
+          res.statusCode=200;
           result = key;
           res.json({status:'Success',data:result});
         }
         else {
+          res.statusCode=400; //Not found 404 
           result='name or email error';
           res.json({status:'Failure',data:result});
         }
@@ -60,14 +62,13 @@ router.route('/generate')
 /* API */
 router.route('/api')
 .get((req,res,next) => {
-        res.statusCode=200;
+        res.statusCode=400; //Bad request 400 as post operation not allowed here
         res.setHeader('Content-Type','application/json');
         result="Please Do Post Here with 'api_key' and image_url";
         res.json({status:'Failure',data:result});
 })
 .post(async (req, res, next) => {
         req.props = Object.assign(req.query, req.params, req.body);
-        res.statusCode=200;
         res.setHeader('Content-Type','application/json');
         let result='';
         console.log(req.props);
@@ -75,20 +76,24 @@ router.route('/api')
         if( await searchKey(req.props.api_key)){
 
             if(await isImageURL(req.props.image_url)){
+                res.statusCode=200;
                 result=await doOCR(req.props.image_url);
                 res.json({status:'Success',data:result});
             }
             else{
+                res.statusCode=400; //Bad request 400 as image is invalid
                 result='Invalid Image';
                 res.json({status:'Failure',data:result});
             }
           
         }
         else{
+          res.statusCode=400; //Bad request 400 as key is invalid
           result='Invalid Key';
           res.json({status:'Failure',data:result});
         }
       } catch (err) {
+        res.statusCode=500; //Internal Error 500 as the server encountered an unexpected condition which prevented it from fulfilling the request.
         result='Internal Server error';
         res.json({status:'Failure',data:result, err});
       }
